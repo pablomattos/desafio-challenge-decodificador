1b9a2a43580d8992c2985159adf39ef036eaf9e8
Pablo Moraes De Mattos <pablomoraesdemattos@gmail.com>
2024-02-09 23:02:30 -0300

    Entrega de projeto

---
+++ Readme.md
@@ -0,0 +1,46 @@
+
+# Decodificador de texto
+
+## Bem-vindo ao projeto de decodificar textos! 
+## O projeto desenvolvido tem suas principais funcionalidades de fazer a criptografa e a descriptografa textos de acordo com algumas regras estabelecidas.
+
+Exemplo
+"gato" => "gaitober"
+gaitober" => "gato"
+
+## Descrição
+### Regras da criptografia:
+
+* A letra "e" é convertida para "enter"
+* A letra "i" é convertida para "imes"
+* A letra "a" é convertida para "ai"
+* A letra "o" é convertida para "ober"
+* A letra "u" é convertida para "ufat"
+
+
+
+### No programa há um espaço em que o usuário pode digitar um texto criptografado ou descriptografado, e a partir dele, pode-se executar várias funções através dos botões:
+
+* Criptogragar: converter um texto para a versão criptografada.
+* Descriptografar: pode-se retornar uma palavra criptografada para a versão inicial.
+* Copiar: faz a cópia do texto (para a área de transferência) que foi inserido, seja ele criptografado ou descriptografado. 
+* Reiniciar: caso o usuário queira limpar todos os campos , ele pode reiniciar o aplicativo para o seu estado inicial.    
+
+
+
+
+## Como Usar
+### Para utilizar o descodificador de texto, basta seguir os seguintes passos:
+
+1. Acesse a página da aplicação clicando aqui.
+2. Insira o texto que deseja criptografar ou descriptografar no campo correspondente.
+3. Selecione a opção de criptografia ou descriptografia.
+4. Clique no botão "Criptografar" ou "Descriptografar", dependendo da opção selecionada.
+5. O resultado da operação será exibido no lado direito da tela.
+6. Se desejar copiar o resultado para a área de transferência, clique no botão "Copiar".
+
+
+Tecnologias Utilizadas
+HTML
+CSS
+JavaScript
---
+++ app.js
@@ -0,0 +1,141 @@
+let txt = document.getElementById('txt');
+let ext = document.getElementById('ext');
+let imagem = document.getElementById('imagem');
+let mensagemSemTexto = document.getElementById('secao__dois__mensagem__sem__texto');
+let mensagemSemTexto2 = document.getElementById('secao__dois__mensagem__sem__texto__2');
+let botaoCriptografar = document.getElementById("criptografar");
+let botaoDescriptografar = document.getElementById("descriptografar");
+let botaoCopiar = document.getElementById("copiar");
+
+function possuiLetrasMaiusculas(texto) {
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] >= 'A' && texto[i] <= 'Z') {
+      return true;
+    }
+  }
+  return false;
+}
+
+function possuiAcentos(texto) {
+  const regexCaracteresEspeciais = /[áéíóúãõâêîôûàèìòùäëïöüñ^~´`#$%¨&*()_/\|+=ºª@]/i;
+  return regexCaracteresEspeciais.test(texto);
+}
+
+function verificarTexto(texto) {
+  return texto.trim() === '';
+}
+
+function exibirMensagemErro(mensagem) {
+  ext.value = mensagem;
+  imagem.style.backgroundImage = 'none';
+  imagem.style.color = 'initial';
+  botaoCriptografar.disabled = false;
+  botaoDescriptografar.disabled = false;   
+  botaoCopiar.disabled = true;
+  mensagemSemTexto.value = '';
+  mensagemSemTexto2.value = '';
+}
+
+function semMensagem(){
+    ext.value = '';
+    imagem.style.backgroundImage = "url('./assets/sem_texto.svg')";
+    imagem.style.backgroundSize = '100% 100%';
+    imagem.style.backgroundPosition = 'center';
+    imagem.style.color = 'transparent'; 
+    imagem.style.maxWidth = '336px';
+    imagem.style.maxHeight = '304px';
+    mensagemSemTexto.value = 'Nenhuma mensagem encontrada';
+    mensagemSemTexto2.value = 'Digite um texto que você deseja criptografar ou descriptografar.'; 
+    botaoCriptografar.disabled = false;
+    botaoDescriptografar.disabled = false;
+    botaoCopiar.disabled = true;
+}
+
+function atualizarTextoExibicao() {
+  ext.value = txt.value;
+  imagem.style.backgroundImage = 'none';
+  imagem.style.color = 'initial';
+}
+
+function reiniciar(){
+  txt.value = '';
+  ext.value = '';
+  imagem.style.backgroundImage = 'none';
+  imagem.style.color = 'initial';
+  botaoCriptografar.disabled = false;
+  botaoDescriptografar.disabled = false;   
+  botaoCopiar.disabled = false;
+  mensagemSemTexto.value = '';
+  mensagemSemTexto2.value = '';
+}
+
+function criptografar() {
+  if (verificarTexto(txt.value)) {
+      semMensagem();
+
+  } else if (!possuiLetrasMaiusculas(txt.value) && !possuiAcentos(txt.value) && !verificarTexto(txt.value)) {
+    const substituicoes = {
+      'e': 'enter',
+      'i': 'imes',
+      'a': 'ai',
+      'o': 'ober',
+      'u': 'ufat'
+    };
+
+    const caracteres = txt.value.split('');
+    for (let i = 0; i < caracteres.length; i++) {
+      const substituicao = substituicoes[caracteres[i]];
+      if (substituicao) {
+        caracteres[i] = substituicao;
+      }
+    }
+    botaoCriptografar.disabled = true;
+    botaoDescriptografar.disabled = false;
+    mensagemSemTexto.value = '';
+    mensagemSemTexto2.value = '';
+    botaoCopiar.disabled = false;
+    txt.value = caracteres.join('');
+    atualizarTextoExibicao();
+  } else {
+    exibirMensagemErro('Não foi possível decodificar esse texto pois ele tem acentos gráficos ou letras maiúsculas!');
+    
+  }
+  
+}
+
+function descriptografar() {
+  if (verificarTexto(txt.value)) {
+    semMensagem();
+    
+
+  } else if (!possuiLetrasMaiusculas(txt.value) && !possuiAcentos(txt.value) && !verificarTexto(txt.value)) {
+    const substituicoesInvertidas = {
+      'enter': 'e',
+      'imes': 'i',
+      'ai': 'a',
+      'ober': 'o',
+      'ufat': 'u'
+    };
+    botaoDescriptografar.disabled = true;
+    botaoCriptografar.disabled = false;
+    mensagemSemTexto.value = '';
+    mensagemSemTexto2.value = '';
+    botaoCopiar.disabled = false;
+    txt.value = txt.value.replace(/enter|imes|ai|ober|ufat/g, match => substituicoesInvertidas[match]);
+    ext.value = txt.value;
+    atualizarTextoExibicao();
+  } else {
+    exibirMensagemErro('Não foi possível decodificar esse texto pois ele tem acentos gráficos ou letras maiúsculas!');    
+  }
+  
+}
+
+function copiar() {
+  var textoCopiado = document.getElementById('ext');
+
+  textoCopiado.select();
+  textoCopiado.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(textoCopiado.value);
+
+  alert("Texto copiado para a área de transferência");
+}
